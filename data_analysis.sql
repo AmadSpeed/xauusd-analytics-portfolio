6459f08fde5bec13cd2fed4aceda2cf474eb18e1
AmadSpeed <arahmattullah63@gmail.com>
2026-07-08 23:45:57 +0700

    membuat query untuk menampilkan daily_return_precentage

--- data_analysis.sql
+++ data_analysis.sql
@@ -1,7 +1,41 @@
-SELECT 
-    time_raw,
-    close_price,
-    AVG(close_price) OVER (ORDER BY time_raw ROWS BETWEEN 11 PRECEDING AND CURRENT ROW) AS ma_1hour,
-    AVG(close_price) OVER (ORDER BY time_raw ROWS BETWEEN 143 PRECEDING AND CURRENT ROW) AS ma_12hour
+WITH ranked_data AS (
+    SELECT 
+        DATE(time_raw) AS trade_date,
+        time_raw,
+        open_price,
+        high_price,
+        low_price,
+        close_price,
+        volume,
+        ROW_NUMBER() OVER (PARTITION BY DATE(time_raw) ORDER BY time_raw DESC) AS rn
     FROM xauusd_raw
-    ORDER BY time_raw DESC;
+),
+
+daily_market_stats AS (
+    SELECT 
+        trade_date,
+        close_price AS daily_close,
+        COALESCE(
+            LAG(close_price, 1) OVER (ORDER BY trade_date ASC), 
+            close_price
+        ) AS yesterday_close,
+        COALESCE(
+            ((close_price - LAG(close_price, 1) OVER (ORDER BY trade_date ASC)) / LAG(close_price, 1) OVER (ORDER BY trade_date ASC) * 100), 
+            0
+        ) AS daily_return_percentage
+    FROM ranked_data
+    WHERE rn = 1
+)
+
+SELECT 
+    r.time_raw AS trade_time,
+    r.close_price,
+    d.daily_close,
+    d.yesterday_close,
+    d.daily_return_percentage,
+    
+    AVG(r.close_price) OVER (ORDER BY r.time_raw ASC ROWS BETWEEN 11 PRECEDING AND CURRENT ROW) AS ma_1hour,
+    AVG(r.close_price) OVER (ORDER BY r.time_raw ASC ROWS BETWEEN 143 PRECEDING AND CURRENT ROW) AS ma_12hour
+FROM ranked_data r
+JOIN daily_market_stats d ON r.trade_date = d.trade_date
+ORDER BY r.time_raw DESC;
